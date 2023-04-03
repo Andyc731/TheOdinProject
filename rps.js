e9f2f609c31f2bde319c2006b6186c1889b70c31
Andy <andychc731@gmail.com>
2023-04-03 15:22:57 -0600

    Add compare function

--- rps.js
+++ rps.js
@@ -12,3 +12,38 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function compare(playerSelection, computerSelection){
+    let result = " ";
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            result = "You Tied!";
+        } else if (computerSelection === "paper") {
+            result = "You Lose!";
+        } else {
+            result = "You Win!";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            result = "You Win!";
+        } else if (computerSelection === "paper") {
+            result = "You Tied!";
+        } else {
+            result = "You Lose!";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            result = "You Lose!";
+        } else if (computerSelection === "paper") {
+            result = "You Win!";
+        } else {
+            result = "You Tied!";
+        }
+    } else {
+        result = "Invalid input";
+    }
+    console.log(result);
+}
+
+compare(prompt("Enter rock, paper or scissors"), getComputerChoice());
