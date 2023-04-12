16af4da234c97b3ba6230d047f1ed6b65c345b2a
Andy <andychc731@gmail.com>
2023-04-12 15:20:54 -0600

    Edit to calculator shape

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +16,26 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    return add(num1, num2);
+}
+
+const digits = document.querySelector('.digits');
+for (let i = 1; i < 13; i++) {
+    const btn = document.createElement('button');
+    btn.setAttribute("id", `num${i}`);
+    digits.appendChild(btn);
+    btn.textContent = `${i}`;
+    if (i === 12) {
+        btn.setAttribute("id", "equals");
+        btn.textContent = "=";
+    } else if (i === 11) {
+        btn.setAttribute("id", "dot");
+        btn.textContent = ".";
+    } else if (i === 10) {
+        btn.setAttribute("id", "num0");
+        btn.textContent = "0";
+    }
 }
