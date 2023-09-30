071ae92e51d10f0f1cfe710739c7f9a94af2e250
Andy <andychc731@gmail.com>
2023-09-30 17:48:52 -0600

    Add content to createHours and display on html

--- src/modules/home.js
+++ src/modules/home.js
@@ -5,40 +5,52 @@ function createHeader() {
     return header;
 }
 
-function createInfoContainer() {
-    const container = document.createElement('div');
-    container.classList.add('container');
-
-    container.appendChild(createIntro());
-
-    return container;
-}
 
 function createIntro() {
     const intro = document.createElement('div');
     intro.classList.add('intro');
-
-    intro.textContent = "We koreans. We cook delicious korean food. Yay koreans. Yes Bibimbap. Yes Gukbap, Yes Kimchi. Food is love"
-
+    
+    intro.textContent = "We koreans. We cook delicious korean food. Yay koreans. Yes Bibimbap. Yes Gukbap, Yes Samgyeopsal, Yes Kimchi. Food is love"
+    
     return intro;
 }
 
 function createHours() {
-
+    const hourContainer = document.createElement('div');
+    hourContainer.classList.add('hours');
+    
+    const text = "Monday-Friday 9am-9pm. Saturday 11am-8pm. Sunday 11am-8pm".split(".");
+    text.forEach((item) => {
+        const hours = document.createElement('p');
+        hours.textContent = item;
+        hourContainer.appendChild(hours);
+    })
+    
+    return hourContainer;
 }
 
 function createLocation() {
+    
+}
 
+function createInfoContainer() {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    
+    container.appendChild(createIntro());
+    container.appendChild(createHours());
+
+    return container;
 }
 
 function createHomepage() {
     const content = document.getElementById('content');
-
+    
     const header = createHeader();
     content.appendChild(header);
-
+    
     content.appendChild(createInfoContainer());
-
+    
 };
 
 export default createHomepage;
