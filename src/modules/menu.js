e291213588b4ad802056ccf31f56aee5e3d995be
Andy <andychc731@gmail.com>
2023-10-05 17:52:01 -0600

    Add all 6 menu items and add styles to them

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,7 +1,13 @@
 function createMenuContainer() {
     const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menu-container');
 
     menuContainer.appendChild(createMenuItem('bibimbap'));
+    menuContainer.appendChild(createMenuItem('bossam'));
+    menuContainer.appendChild(createMenuItem('bulgogi'));
+    menuContainer.appendChild(createMenuItem('gopchang'));
+    menuContainer.appendChild(createMenuItem('naengmyeon'));
+    menuContainer.appendChild(createMenuItem('kimchi'));
 
     return menuContainer;
 }
