8ba6683021a1b3b4c9e3fb2a6e7a6b893f8a0b89
Andy <andychc731@gmail.com>
2023-10-07 17:54:48 -0600

    Add images to contacts

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -15,29 +15,32 @@ function createContactContainer() {
 
     contactContainer.appendChild(createContact(
         'Baek Jong Won',
-        'Lovely Man',
+        'Chef, Lovely Man',
         '333-444-5555',
-        'baekjongwonsemail@baekjongwon.com'
+        'baekjongwonsemail@baekjongwon.com',
+        '../../images/baek.png'
     ));
 
     contactContainer.appendChild(createContact(
         'Gordon Ramsay',
         'Waiter',
         '444-333-2222',
-        'gordonramsaysemail@gordonramsay.com'
+        'gordonramsaysemail@gordonramsay.com',
+        '../../images/gordon.png'
     ))
 
     contactContainer.appendChild(createContact(
         'Patrick',
         'Phone Duty',
         '222-444-3333',
-        'thisispatrick@thisispatrick.com'
+        'thisispatrick@thisispatrick.com',
+        '../../images/patrick.png'
     ))
     
     return contactContainer;
 }
 
-function createContact(name, position, phoneNum, email) {
+function createContact(name, position, phoneNum, email, imageURL) {
     const contact = document.createElement('div');
     contact.classList.add('contact');
 
@@ -45,9 +48,18 @@ function createContact(name, position, phoneNum, email) {
     contact.appendChild(createDiv('position', position));
     contact.appendChild(createDiv('phoneNum', phoneNum));
     contact.appendChild(createDiv('email', email));
+    contact.appendChild(createImg(imageURL))
 
     return contact;
 
+}
+
+function createImg(imgURL) {
+    const image = document.createElement('img');
+    image.classList.add('contact-image');
+    image.src = imgURL;
+
+    return image;
 }
 
 function createDiv(contactClass, content) {
