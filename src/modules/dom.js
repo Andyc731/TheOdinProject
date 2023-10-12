bed7fbfcbbb62da9f0435aff49d662cd4affbf90
Andy <andychc731@gmail.com>
2023-10-12 17:59:38 -0600

    Move dom stuff to html, since it is easier

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,19 +1,2 @@
-function createDiv(divClass) {
-    const blah = document.createElement('div');
-    blah.classList.add(divClass);
-
-    return blah;
-}
-
-function createPage() {
-    const page = document.getElementById('page');
-    page.appendChild(createDiv('header'));
-    page.appendChild(createDiv('sidebar'));
-    page.appendChild(createDiv('content'));
-
-    console.log('blah');
-
-    return page;
-}
 
 export default createPage;
