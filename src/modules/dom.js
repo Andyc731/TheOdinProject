83e93b1e58a88ed002052e08711900233e4c2705
Andy <andychc731@gmail.com>
2023-10-13 17:37:35 -0600

    Add eventListener for tabs for active tab

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,2 +1,25 @@
+function createPage() {
+    const tabs = document.querySelectorAll('.tab');
+
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', () => {
+            if (!tab.classList.contains('active')) {
+                eventListenerForTab(tab);
+            }
+        })
+})
+}
+
+function eventListenerForTab(tab) {
+    const buttonsArray = document.querySelectorAll('.tab');
+
+    buttonsArray.forEach((tab) => {
+        if (tab !== this) {
+            tab.classList.remove('active');
+        }
+    })
+
+    tab.classList.add('active');
+}
 
 export default createPage;
