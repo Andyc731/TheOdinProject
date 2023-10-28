cb1b245e6be0b61fe2d5771005604ab6bdce7274
Andy <andychc731@gmail.com>
2023-10-28 11:10:23 -0600

    move createPage function from content.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import createPage from "./modules/dom";
+import createContent from "./modules/content";
+import createTabs from "./modules/tab";
+import createSidebar from "./modules/sidebar";
+
+function createPage() {
+    createContent();
+    createTabs();
+    createSidebar();
+}
 
 createPage();
