7047d1f90162d88e383c405921c445b98350e8a9
Andy <andychc731@gmail.com>
2023-10-28 10:42:53 -0600

    rename file to sidebar

---
+++ src/modules/sidebar.js
@@ -0,0 +1,66 @@
+function tagsOnClick(tags) {    
+    tags.forEach((tag) => {
+        tag.addEventListener('click', () => {
+            setActiveOnClick(tag);
+        })
+    })
+}
+
+function setActiveOnClick(currentTag) {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach((tag) => {
+        if (tag !== this) tag.classList.remove('active');
+    })
+    currentTag.classList.add('active');
+}
+
+function showDialogOnClick(div, dialogID) {
+    const dialog = document.getElementById(dialogID);
+    div.addEventListener('click', () => {
+    dialog.showModal();
+    })
+
+    dialog.addEventListener('click', e => {
+    const dialogDimensions = dialog.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+        ) {
+            dialog.close();
+        }
+    })
+
+    dialogButtonOnClick();
+}
+
+function dialogButtonOnClick() {
+    const sidebar = document.querySelector('.sidebar');
+    const addTag = document.querySelector('.addTag');
+    const addTodoButton = document.querySelector('.addTagButton');
+    addTodoButton.addEventListener('click', () => {
+        const name = document.getElementById('tag-name').value;
+        sidebar.insertBefore(createTag(name), addTag);
+
+        document.getElementById('addTagDialog').close();
+        document.getElementById('tagForm').reset();
+    })
+}
+
+function createTag(name) {
+    const div = document.createElement('div');
+    div.textContent = name;
+    div.classList.add(name, 'tag');
+
+    const tags = document.querySelectorAll('.tag');
+    tagsOnClick(tags);
+
+    return div;
+}
+
+function tag() {
+    showDialogOnClick(document.querySelector('.addTag'), 'addTagDialog');
+}
+
+export default tag;
