86db24ca29caf6b948fc3e1b11d78380cd04bec3
Andy <andychc731@gmail.com>
2024-01-02 12:01:10 -0700

    Add regex for email input

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const myForm = document.getElementById('form');
+const submitButton = document.getElementById('submit');
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const zip = document.getElementById('zip');
+const password = document.getElementById('password');
 
-myForm.addEventListener('submit', submitHandler);
+const inputs = [email, country, zip, password];
 
-function submitHandler(e){
-    e.preventDefault();
+submitButton.addEventListener('click', () => {
+    inputs.forEach(item => {
+        if (checkEmpty(item)) {
 
+        }
+    })
+})
 
+email.addEventListener('input', () => {
+    if (!email.value.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )) {
+            console.log ('input does not match format');
+        }
+})
+
+function checkEmpty(input) {
+    return input.value === '' ? true : false;
 }
