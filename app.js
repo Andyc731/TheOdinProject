4f35b5de50b2259ac454199d4804ac6a6348a570
Andy <andychc731@gmail.com>
2024-01-02 14:38:21 -0700

    change to validity with constraint API

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ const email = document.getElementById('email');
 const country = document.getElementById('country');
 const zip = document.getElementById('zip');
 const password = document.getElementById('password');
+const myForm = document.getElementById('form');
 
 const inputs = [email, country, zip, password];
 
-submitButton.addEventListener('click', () => {
-    inputs.forEach(item => {
-        if (checkEmpty(item)) {
-
-        }
-    })
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 })
 
 email.addEventListener('input', () => {
-    if (!email.value.match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        )) {
-            console.log ('input does not match format');
-        }
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("ex: example@example.com");
+        email.reportValidity();
+    } else {
+        email.setCustomValidity('');
+    }
+})
+
+country.addEventListener('input', () => {
+    if (!country.value.match(/^[a-zA-Z]{2,}/)) {
+        country.setCustomValidity('ex: Canada');
+        country.reportValidity();
+    } else {
+        country.setCustomValidity('');
+    }
+})
+
+zip.addEventListener('input', () => {
+    if (!zip.value.match(/(^\d{5}$)|(^\d{5}-\d{4}$)/)) {
+        zip.setCustomValidity('ex: 11111');
+        zip.reportValidity();
+    } else {
+        zip.setCustomValidity('');
+    }
+})
+
+password.addEventListener('input', () => {
+    if (!password.value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*,.])[A-Za-z\d@$!%*?&,.]{6,20}$/)){
+        password.setCustomValidity('must include lowercase letter, uppercase letter, number, and special character. length 6-20')
+        password.reportValidity();
+    } else {
+        password.setCustomValidity('');
+    }
 })
 
 function checkEmpty(input) {
