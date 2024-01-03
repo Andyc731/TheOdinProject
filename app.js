77260a0c42dc1ba1d23bbbcca593ebbff8056139
Andy <andychc731@gmail.com>
2024-01-03 10:49:41 -0700

    Add search for new gif

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const img = document.querySelector('img');
 const btn = document.getElementById('fetchBtn')
+const formBtn = document.getElementById('formBtn');
+const search = document.getElementById('search');
+const form = document.getElementById('myForm');
 
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=1GKAaRMDOKUH1DaMVN5XYY5YKQJct6oR&s=cats', {mode: 'cors'})
     .then(function(response) {
@@ -17,5 +20,20 @@ btn.addEventListener('click', () => {
     img.src = response.data.images.original.url;
   }
 );
-console.log('blah');
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+formBtn.addEventListener('click', () => {
+    const input = search.value;
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=1GKAaRMDOKUH1DaMVN5XYY5YKQJct6oR&s=${input}`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    }).then(function(response) {
+        img.src = response.data.images.original.url;
+    }).catch(function() {
+        console.log('empty input');
+    })
 })
