611d76182e6a284dd4fdd00118143ba2c7f863e0
Andy <andychc731@gmail.com>
2024-01-03 14:39:27 -0700

    add switch with animations. does nothing atm

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const myForm = document.getElementById('myForm')
 const locationInput = document.getElementById('location');
+const myImg = document.getElementById('myImg');
+const temperature = document.getElementById('temperature');
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -10,13 +12,24 @@ async function weatherAPI(location) {
     fetch(`https://api.weatherapi.com/v1/current.json?key=623ae80523c24099aad193932240301&q=${location}`, {mode: "cors"})
     .then(response => {
         return response.json();
-    }).then(response => {
+    })
+    .then(response => {
+        myImg.src = response.current.condition.icon;
+        temperature.textContent = `${response.current.temp_c}` + '\u2103';
         console.log(response);
+    })
+    .catch(error => {
+        locationInput.setCustomValidity('Location Not Found');
     })
 }
 
-locationInput.addEventListener('keypress', (e) => {
+locationInput.addEventListener('keyup', (e) => {
+    if (locationInput.value.match(/[a-zA-Z]{2,}/)) {
+        locationInput.setCustomValidity('');
+    }
+
     if (e.key === 'Enter') {
         weatherAPI(locationInput.value);
     }
+
 })
