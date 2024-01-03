2d35ec79f7edf9fc1b79b84cf3294ef8c60fd62a
Andy <andychc731@gmail.com>
2024-01-03 10:32:54 -0700

    Add a button to fetch gif

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const img = document.querySelector('img');
+const btn = document.getElementById('fetchBtn')
 
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=1GKAaRMDOKUH1DaMVN5XYY5YKQJct6oR&s=cats', {mode: 'cors'})
     .then(function(response) {
     return response.json();
   }).then(function(response) {
     img.src = response.data.images.original.url;
-  });
+  }
+);
+
+btn.addEventListener('click', () => {
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=1GKAaRMDOKUH1DaMVN5XYY5YKQJct6oR&s=cats', {mode: 'cors'})
+    .then(function(response) {
+    return response.json();
+  }).then(function(response) {
+    img.src = response.data.images.original.url;
+  }
+);
+console.log('blah');
+})
