c646f5d8cadf0f11c3f27f10512a545086bda96c
Andy <andychc731@gmail.com>
2024-01-03 13:05:33 -0700

    add input that fetches data

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const myForm = document.getElementById('myForm')
+const locationInput = document.getElementById('location');
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
 async function weatherAPI(location) {
 
     fetch(`https://api.weatherapi.com/v1/current.json?key=623ae80523c24099aad193932240301&q=${location}`, {mode: "cors"})
@@ -7,3 +14,9 @@ async function weatherAPI(location) {
         console.log(response);
     })
 }
+
+locationInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        weatherAPI(locationInput.value);
+    }
+})
