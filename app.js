a87684fd8d9a60572d5fff115864bfcebd96cef1
Andy <andychc731@gmail.com>
2024-01-04 11:26:59 -0700

    added global variable that stores data from API

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 const myForm = document.getElementById('myForm')
-const locationInput = document.getElementById('location');
+const searchInput = document.getElementById('search');
 const myImg = document.getElementById('myImg');
 const temperatureDiv = document.getElementById('temperature-div');
 const unitSwitch = document.querySelector('.switch');
 const unitText = document.getElementById('unit');
 
-const temperature = [0, 0];
-let unit = 'celsius';
+let weatherData;
+
+weatherAPI('london');
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 
 async function weatherAPI(location) {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=623ae80523c24099aad193932240301&q=${location}`, {mode: "cors"})
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-        myImg.src = response.current.condition.icon;
-        temperature[0] = response.current.temp_c;
-        temperature[1] = response.current.temp_f;
-        if (unit === 'celsius') {
-            temperatureDiv.textContent = temperature[0] + '\u2103';
-        } else {
-            temperatureDiv.textContent = temperature[1] + '\u2109';
-        }
-    })
-    .catch(error => {
-        locationInput.setCustomValidity('Location Not Found');
-    })
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=623ae80523c24099aad193932240301&q=${location}`, {mode: "cors"});
+    weatherData = await response.json();
+    console.log(weatherData)
+    displayWeather(weatherData, 'celsius');
+}
+
+function displayWeather(data, unit) {
+    myImg.src = data.current.condition.icon;
+    if (unit === 'celsius') {
+        temperatureDiv.textContent = weatherData.current.temp_c + '\u2103';
+    } else if (unit === 'fahrenheit') {
+        temperatureDiv.textContent = weatherData.current.temp_f + '\u2109';
+    }
 }
 
 
-locationInput.addEventListener('keyup', (e) => {
-    if (locationInput.value.match(/[a-zA-Z]{2,}/)) {
-        locationInput.setCustomValidity('');
+// async function displayWeather(location, unit) {
+//     const weatherData = await weatherAPI(location);
+//     myImg.src = weatherData.current.condition.icon;
+//     if (unit === 'celsius') {
+//         temperatureDiv.textContent = weatherData.current.temp_c + '\u2103';
+//     } else if (unit === 'fahrenheit') {
+//         temperatureDiv.textContent = weatherData.current.temp_f + '\u2109';
+//     }
+
+// }
+
+
+searchInput.addEventListener('keyup', (e) => {
+    if (searchInput.value.match(/[a-zA-Z]{2,}/)) {
+        searchInput.setCustomValidity('');
     }
 
     if (e.key === 'Enter') {
-        weatherAPI(locationInput.value)
+        weatherAPI(searchInput.value)
         myImg.src = 'image/loading.gif';
     }
 })
@@ -47,12 +56,10 @@ locationInput.addEventListener('keyup', (e) => {
 unitSwitch.addEventListener('change', () => {
     if (unitText.textContent === 'celsius') {
         unitText.textContent = 'fahrenheit';
-        temperatureDiv.textContent = temperature[1] + '\u2109';
-        unit = 'fahrenheit';
+        displayWeather(weatherData, 'fahrenheit');
     } else {
         unitText.textContent = 'celsius';
-        temperatureDiv.textContent = temperature[0] + '\u2103';
-        unit = 'celsius';
+        displayWeather(weatherData, 'celsius');
     }
 })
 
