8e463a2a7cbdb9611e8b4b7c0f1331d1b90705c7
Andy <andychc731@gmail.com>
2024-01-04 14:12:11 -0700

    add displayForecastWeather function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const feel = document.getElementById('feel');
 const humidity = document.getElementById('humidity');
 const rainChance = document.getElementById('rain-chance');
 const windSpeed = document.getElementById('wind-speed');
-
+const days = document.querySelectorAll('.days');
 
 weather();
 
@@ -19,24 +19,39 @@ myForm.addEventListener('submit', (e) => {
 function weather() {
     let weatherData;
 
+    weatherAPI('london');
+
     async function weatherAPI(location) {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=623ae80523c24099aad193932240301&q=${location}`, {mode: "cors"});
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=623ae80523c24099aad193932240301&days=7&q=${location}`, {mode: "cors"});
         weatherData = await response.json();
         console.log(weatherData)
-        displayWeather(weatherData, 'celsius');
+        displayCurrentWeather(weatherData, 'celsius')
+        displayForecastWeather(weatherData, 'celsius');
     }
-
-    weatherAPI('london');
     
-    function displayWeather(data, unit) {
+    function displayCurrentWeather(data, unit) {
         myImg.src = data.current.condition.icon;
         if (unit === 'celsius') {
             temperatureDiv.textContent = weatherData.current.temp_c + '\u2103';
+            feel.textContent = 'feels like ' + data.current.feelslike_c;
+            windSpeed.textContent = 'wind speed: ' + data.current.wind_kph;
         } else if (unit === 'fahrenheit') {
             temperatureDiv.textContent = weatherData.current.temp_f + '\u2109';
+            feel.textContent = 'feels like ' + data.current.feelslike_f;
+            windSpeed.textContent = 'wind speed: ' + data.current.wind_mph;
         }
-        feel
+        humidity.textContent = 'humidity: ' +data.current.humidity + '%';
     }
+
+    function displayForecastWeather(data, unit) {
+        const daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        for(i in weatherData.forecast.forecastday){
+            const dayOfWeek = new Date(weatherData.forecast.forecastday[i].date).getDay();
+            console.log(daysInWeek[dayOfWeek]);
+            days[i].textContent = daysInWeek[dayOfWeek];
+        }
+    };
 
     searchInput.addEventListener('keyup', (e) => {
         if (searchInput.value.match(/[a-zA-Z]{2,}/)) {
@@ -44,7 +59,7 @@ function weather() {
         }
     
         if (e.key === 'Enter') {
-            weatherAPI(searchInput.value)
+            weatherAPI(searchInput.value);
             myImg.src = 'image/loading.gif';
         }
     })
@@ -52,10 +67,10 @@ function weather() {
     unitSwitch.addEventListener('change', () => {
         if (unitText.textContent === 'celsius') {
             unitText.textContent = 'fahrenheit';
-            displayWeather(weatherData, 'fahrenheit');
+            displayCurrentWeather(weatherData, 'fahrenheit');
         } else {
             unitText.textContent = 'celsius';
-            displayWeather(weatherData, 'celsius');
+            displayCurrentWeather(weatherData, 'celsius');
         }
     })
 }
