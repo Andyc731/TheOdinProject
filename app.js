8a876a90e6b8d3ae45ffa234d15fa6a35cf38ca1
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-09 15:28:12 -0700

    add methods to LinkedList up to contains

---
+++ app.js
@@ -0,0 +1,100 @@
+function Node(value = null) {
+    return {value: value, nextNode: null};
+}
+
+const blah = Node('blah');
+const list = LinkedList();
+
+list.append('blah');
+list.prepend('blah2');
+list.append('blah3');
+list.pop();
+list.pop();
+
+console.log(list.at(0));
+
+function LinkedList() {
+
+    return {
+        listHead: null,
+
+        append: function (value){
+            const newNode = Node(value);
+
+            if (this.listHead) {
+                let current = this.listHead;
+                while (current.nextNode !== null) {
+                    current = current.nextNode;
+                }
+                current.nextNode = newNode;
+            } else {
+                this.listHead = newNode;
+            }
+        },
+    
+        prepend: function (value){
+            const newNode = Node(value);
+
+            newNode.nextNode = this.listHead;
+            this.listHead = newNode;
+        },
+    
+        size: function () {
+            let count = 0;
+            let current = this.listHead;
+            while(current !== null) {
+                current = current.nextNode;
+                count++;
+            }
+
+            return count;
+        },
+    
+        head: function () {
+            return this.listHead;
+        },
+
+        tail: function () {
+            if (this.listHead) {
+                let current = this.listHead;
+                while (current.nextNode !== null) {
+                    current = current.nextNode;
+                }
+                return current;
+            } else {
+                return this.listHead;
+            }
+        },
+    
+        at: function (index) {
+            let current = this.listHead;
+            for (let i = 0; i < index; i++) {
+                if(!current.nextNode) {
+                    return null;
+                }
+                current = current.nextNode;
+            }
+            return current;
+        },
+    
+        pop: function () {
+            this.at(this.size()-2).nextNode = null;
+        },
+    
+        contains: function (value) {
+            let current = this.listHead;
+            for(let i = 0; i < this.size(); i++) {
+                if(current.value === value) {
+                    return true;
+                }
+            }
+            return false;
+        },
+    
+        find: function (value) {},
+    
+        toString: function () {},
+
+    }
+
+ }
