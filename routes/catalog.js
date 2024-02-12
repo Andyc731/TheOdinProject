dbc74a729dc542d7f540faa1c6337651cd1eaa8d
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-12 15:56:30 -0700

    change to view pug

--- routes/catalog.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const route = express.Router();
-
-const item_controller = require("../controllers/itemController.js");
-const category_controller = require("../controllers/categoryController.js");
-
-route.get("/", item_controller.index);
