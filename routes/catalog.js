14da916ccd504cb77e5834f77295014aab9f998b
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-12 21:55:28 -0700

    add routes for category

--- routes/catalog.js
+++ routes/catalog.js
@@ -14,4 +14,13 @@ router.get("/items/:id/update", items_controller.item_update_get);
 router.post("/items/:id/update", items_controller.item_update_post);
 router.post("/items/:id", items_controller.item_detail);
 
+router.get("/category", category_controller.category_list);
+router.get("/category/create", category_controller.category_create_get);
+router.get("/category/create", category_controller.category_create_post);
+router.get("/category/:id/delete", category_controller.category_delete_get);
+router.get("/category/:id/delete", category_controller.category_delete_post);
+router.get("/category/:id/update", category_controller.category_update_get);
+router.get("/category/:id/update", category_controller.category_update_post);
+router.get("/category/:id", category_controller.category_detail);
+
 module.exports = router;
