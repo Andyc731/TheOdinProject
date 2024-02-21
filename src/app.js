5f400d3f4815240a5e0de6a13b92e31301a482f9
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-21 11:51:06 -0700

    add routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/user", routes.user);
+app.use("/", routes.blog);
+app.use("/comments", routes.comments);
+app.use("/posts", routes.posts);
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
