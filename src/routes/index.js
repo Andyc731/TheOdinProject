d9933e159bd90c0ee904b88abab6ea36515c94a5
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-21 11:54:23 -0700

    add routes

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import user from "./user";
+import blog from "./blog";
+import comments from "./comments";
+import posts from "./posts";
+
+export default {
+  user,
+  comments,
+  blog,
+  posts,
+};
