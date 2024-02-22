83fade38341207cffe0e240928a1fd05dff519bf
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-22 12:43:06 -0700

    delete routes. not needed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,7 @@
 import user from "./user";
-import blog from "./blog";
-import comments from "./comments";
 import posts from "./posts";
 
 export default {
   user,
-  comments,
-  blog,
   posts,
 };
