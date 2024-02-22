edb9d6e11b5d599818a44367efedafab040f5d0a
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-22 12:43:49 -0700

    implement get functions

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,8 +1,34 @@
 import { Router } from "express";
+import Comment from "../models/comments";
+import Post from "../models/post";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("posts route");
+router.get("/", async (req, res) => {
+  const allPosts = await Post.find().exec();
+  res.send(allPosts);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).exec();
+    res.send("single post");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const comments = await Comment.find({ post: req.params.id }).exec();
+    res.send("post comments");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/:id/comments", (req, res) => {
+  res.send("post comment POST");
 });
 
 export default router;
