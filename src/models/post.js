d1d1aca7be0e0bf36462f626838b02c2f778d880
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-23 11:30:44 -0700

    Change user to type string

--- src/models/post.js
+++ src/models/post.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: String, required: true },
   text: { type: String, required: true },
   date: { type: Date, required: true },
 });
