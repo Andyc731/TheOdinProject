823e567a9a060995b2d2a1d616cce8835394a116
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-02-23 11:31:11 -0700

    implement post for post and comment

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -9,26 +9,51 @@ router.get("/", async (req, res) => {
   res.send(allPosts);
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const post = new Post({
+      user: req.body.user,
+      text: req.body.text,
+      date: new Date(),
+    });
+    await post.save();
+    res.json({ message: "Post created successfully", post: post });
+  } catch (err) {
+    res.send(err);
+  }
+});
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id).exec();
-    res.send("single post");
+    res.json(post);
   } catch (err) {
-    res.send(err);
+    res.status(400).json({ error: "Invalid request parameters" });
   }
 });
 
 router.get("/:id/comments", async (req, res) => {
   try {
     const comments = await Comment.find({ post: req.params.id }).exec();
-    res.send("post comments");
+    res.json(comments);
   } catch (err) {
-    res.send(err);
+    res.status(400).json({ error: "Invalid request parameters" });
   }
 });
 
-router.post("/:id/comments", (req, res) => {
-  res.send("post comment POST");
+router.post("/:id/comments", async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id).exec();
+    const comment = new Comment({
+      text: req.body.text,
+      name: req.body.name,
+      post: post,
+      date: new Date(),
+    });
+
+    res.json({ message: "Comment created successfully", comment: comment });
+  } catch (err) {
+    res.status(400).json({ error: "Invalid request parameters" });
+  }
 });
 
 export default router;
